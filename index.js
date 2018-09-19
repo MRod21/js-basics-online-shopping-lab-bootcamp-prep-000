cd73d1aa8d496d18f9d36e2eb308a059038d0bc2
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2018-09-19 01:10:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,13 +10,14 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var price = Math.floor(100*(Math.random()));
-  var itemObj = {[item]: price};
-  cart.push(itemObj);
-  console.log(`${item} has been added to your cart.`);
-  return cart
-}
+  const price = Math.floor(Math.random() * 100)
 
+  cart.push({ [item]: price })
+
+  console.log(`${item} has been added to your cart.`)
+
+  return cart
+  
 function viewCart() {
   // write your code here
 }
