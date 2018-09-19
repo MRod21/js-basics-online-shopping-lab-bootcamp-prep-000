080730b43b6c8133f080c6c43dc38d7da7942079
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2018-09-19 01:24:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  var price = Math.floor(100*(Math.random()));
-  cart.push({itemObj);
+  cart.push({[item]Obj);
   console.log(`${item} has been added to your cart.`);
   return cart;
 }
